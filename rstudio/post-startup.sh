916bfb0b1c2b67904fbc541c3e8e4befd3192c95
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2023-08-18 15:24:24 -0400

    fix single quote typo

--- rstudio/post-startup.sh
+++ rstudio/post-startup.sh
@@ -200,7 +200,7 @@ emit "Installing GCS fuse..."
 
 apt-get install -y fuse
 
-export GCSFUSE_REPO=gcsfuse-\lsb_release -c -s\
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
