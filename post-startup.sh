72d61bc95f23916f4476af7f4f1450197b4c81d5
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2023-09-27 11:21:36 -0400

    Update post-startup.sh

--- post-startup.sh
+++ post-startup.sh
@@ -184,8 +184,6 @@ fi
 rm -f "${USER_SSH_DIR}/id_rsa.tmp"
 
 # Set the github known_hosts
-apt-get update
-apt-get install -y openssh-client
 ${RUN_AS_USER} "ssh-keyscan -H github.com >> '${USER_SSH_DIR}/known_hosts'"
 
 # Create git repos directory
@@ -205,6 +203,7 @@ emit "Installing GCS fuse..."
 
 apt-get install -y fuse
 
+apt-get install -y lsb-core
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
