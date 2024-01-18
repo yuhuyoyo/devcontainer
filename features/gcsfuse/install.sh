6b6d6d72b577f0bdacc1a62cb13beeb0696a1f00
Yu Hu <yuhuyoyo@google.com>
2024-01-18 16:04:30 -0500

    add gcsfuse as feature

---
+++ features/gcsfuse/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "Installing gcs fuse..."
+
+apt-get update
+apt-get install -y lsb-core
+apt-get install -y fuse
+
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+apt-get update
+apt-get install -y gcsfuse
--- startupscript/post-startup.sh
+++ startupscript/post-startup.sh
@@ -150,21 +150,21 @@ source ${SCRIPT_DIR}/git-setup.sh
 # Mount buckets
 #############################
 # Installs gcsfuse if it is not already installed.
-if ! which gcsfuse >/dev/null 2>&1; then
-  emit "Installing gcsfuse..."
-  # install packages needed to install gcsfuse
-  apt-get install -y \
-    fuse \
-    lsb-core
-
-  # Install based on gcloud docs here https://cloud.google.com/storage/docs/gcsfuse-install.
-  export GCSFUSE_REPO="gcsfuse-$(lsb_release -c -s)" \
-    && echo "deb https://packages.cloud.google.com/apt ${GCSFUSE_REPO} main" | tee /etc/apt/sources.list.d/gcsfuse.list \
-    && curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | apt-key add -
-  apt-get update \
-    && apt-get install -y gcsfuse
-else
-  emit "gcsfuse already installed. Skipping installation."
-fi
+#if ! which gcsfuse >/dev/null 2>&1; then
+#  emit "Installing gcsfuse..."
+#  # install packages needed to install gcsfuse
+#  apt-get install -y \
+#    fuse \
+#    lsb-core
+#
+#  # Install based on gcloud docs here https://cloud.google.com/storage/docs/gcsfuse-install.
+#  export GCSFUSE_REPO="gcsfuse-$(lsb_release -c -s)" \
+#    && echo "deb https://packages.cloud.google.com/apt ${GCSFUSE_REPO} main" | tee /etc/apt/sources.list.d/gcsfuse.list \
+#    && curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | apt-key add -
+#  apt-get update \
+#    && apt-get install -y gcsfuse
+#else
+#  emit "gcsfuse already installed. Skipping installation."
+#fi
 
 ${RUN_AS_LOGIN_USER} "wb resource mount"
