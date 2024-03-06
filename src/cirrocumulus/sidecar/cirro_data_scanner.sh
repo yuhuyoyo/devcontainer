46c1aa5f6f2723badca06a7dff07f08f750bfee9
Yu Hu <yuhuyoyo@google.com>
2024-03-06 17:15:41 -0500

    testing

---
+++ src/cirrocumulus/sidecar/cirro_data_scanner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# cirro_data_scanner scans the shared volumes for data folders with cirro supported file type
+# and create dataset.
+scan_for_cirro_data() {
+  echo "hello"
+}
+
+while true; do
+  scan_files
+  sleep 5
+done
